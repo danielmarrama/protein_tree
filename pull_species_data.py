37931eec147c6e86fce5660ea856ab788158e8af
Daniel Marrama <dmarrama@lji.org>
2023-03-01 12:26:20 -0800

    use MySQL connector

--- pull_species_data.py
+++ pull_species_data.py
@@ -1,35 +1,44 @@
-#!/usr/bin/env python3
+import mysql.connector
+import argparse
 
-import requests
 
 def get_epitopes(taxon_id):
   """
   Get all epitopes for a species from the IEDB API.
   """
-
+  pass
   # TODO: pull data from the MySQL database
 
-  url = f'https://query-api.iedb.org/epitope_search'
 
 def get_sources(taxon_id):
   """
   Get all source antigens for a species from the IEDB API.
   """
-  url = f'https://query-api.iedb.org/parent_protein'
+  pass
 
 
 def main():
   # define command line args which will take in a taxon ID
-  parser = argparse.ArgumentParser(description='Select the taxon ID to get the epitopes and source antigens.')
-  parser.add_argument('taxon_id', help='Taxon ID for the species.')
+  parser = argparse.ArgumentParser()
+  
+  parser.add_argument('-u', '--user', help='User for IEDB MySQL connection.')
+  parser.add_argument('-p', '--password', help='User for IEDB MySQL connection.')
+  
   args = parser.parse_args()
-  taxon_id = args.taxon_id
+  user = args.user
+  password = args.password
 
-  # get all epitopes for the species
-  get_epitopes(taxon_id)
+  connection = mysql.connector.connect(
+    host="iedb-mysql.liai.org",
+    port=33306,
+    user=user,
+    passwd=password,
+    database='iedb_query'
+  )
 
-  # get all source antigens for the species
-  get_sources(taxon_id)
+  c = connection.cursor()
+  c.execute("SHOW TABLES;")
+  print(c.fetchall())
 
 if __name__ == '__main__':
   main()
