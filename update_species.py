56235c6324386a95d44f37b79e52cecf9ac57d14
Daniel Marrama <danielmarrama@gmail.com>
2023-04-19 12:58:08 -0700

    update_species.py stores unknowns

--- update_species.py
+++ update_species.py
@@ -5,7 +5,7 @@ import requests
 import _pickle as pickle
 import pandas as pd
 from sqlalchemy import text
-from sql_engine import create_sql_engine
+from protein_tree.sql_engine import create_sql_engine
 
 
 def update_species_data(user, password):
@@ -24,7 +24,7 @@ def update_species_data(user, password):
   # get all epitope data - map all IDs to species rank
   sql_engine = create_sql_engine(user, password)
   organism_df = pd.DataFrame(sql_engine.connect().execute(text(sql_query)))
-  species_mapping = create_species_mapping(list(organism_df['organism2_id'].astype(str).unique()))
+  species_mapping, unknown_taxons = create_species_mapping(list(organism_df['organism2_id'].astype(str).unique()))
 
   # create dataframe with species ID, species name, and all taxa
   data = [(key, *value) for key, value in species_mapping.items()]
@@ -33,11 +33,22 @@ def update_species_data(user, password):
   # group by Species, Taxon ID, and Species Name, aggregating the lower ranks
   grouped_df = df.groupby(['Species Taxon ID', 'Species Name', 'Group', 'Vertebrate'])['Lower Rank Taxon ID'].apply(lambda x: '; '.join(map(str, x))).reset_index()
   grouped_df = grouped_df.rename(columns={'Species Taxon ID': 'Taxon ID', 'Lower Rank Taxon ID': 'All Taxa'})
-  grouped_df.to_csv(f'new_species_data.csv', index=False)
+  
+  # add unknown taxa
+  grouped_df = group_df.append({
+    'Taxon ID': '',
+    'Species Name': 'Unknown',
+    'All Taxa': '; '.join(unknown_taxons),
+    'Group': '',
+    'Vertebrate': '',
+  }, ignore_index=True)
+  
+  grouped_df.to_csv(f'species.csv', index=False)
 
 def create_species_mapping(taxon_ids):
   """Create mapping from lower rank taxonomy to species."""
   species_mapping = {}
+  unknown_taxons = []
   for taxon_id in taxon_ids:
 
     print(f'Getting data for: {taxon_id}...')
@@ -45,6 +56,7 @@ def create_species_mapping(taxon_ids):
     species_data = get_species_data(taxon_id)
 
     if species_data is None:
+      unknown_taxons.append(taxon_id)
       continue
     
     print(f'Taxon ID: {taxon_id}')
@@ -55,12 +67,12 @@ def create_species_mapping(taxon_ids):
     print(f'Vertebrate: {species_data["vertebrate"]}\n')
 
     species_mapping[taxon_id] = (species_data['taxon_rank'], species_data['species_taxon_id'], species_data['species_name'], species_data['group'], species_data['vertebrate'])
-  
+
   path = os.path.join(os.path.dirname(__file__), '../mappings/species_mapping.pickle')
   with open(path, 'wb') as f:
     pickle.dump(species_mapping, f)
   
-  return species_mapping
+  return species_mapping, unknown_taxons
 
 def get_taxonomy_information(taxon_id):
   """Read in taxonomy information from UniProt."""
