4d603b6dd849f8848a78d765616e6389537c7526
Daniel Marrama <dmarrama@lji.org>
2023-06-22 01:21:17 -0700

    use fixture instead for test

--- protein_tree/tests/test_protein_tree.py
+++ protein_tree/tests/test_protein_tree.py
@@ -1,43 +1,17 @@
+import pytest
 import pandas as pd
-from unittest.mock import patch
-from protein_tree.get_data import DataFetcher
+from pathlib import Path
+
 from protein_tree.select_proteome import ProteomeSelector
 
 
-# Create some dummy data to represent the results from the database query
-dummy_epitopes = pd.DataFrame({
-    'Sequence': ['THEILWPSF'],
-    'Source Name': ['cytochrome c oxidase, subunit III'],
-    'Source Accession': ['ABP77492.1'],
-})
+@pytest.fixture
+def epitopes() -> Path:
+  return Path(__file__).parent / "data" / "epitopes.csv"
 
-dummy_sources = pd.DataFrame({
-    'Accession': ['ABP77492.1'],
-    'Name': ['cytochrome c oxidase, subunit III'],
-})
 
-# Patch the get_epitopes and get_sources methods in the DataFetcher class
-# VPN is required to access the database so we don't want to actually run
-# the queries in the tests
-@patch('protein_tree.get_data.DataFetcher')
-def test_get_data(MockDataFetcher):
-  mock_fetcher = MockDataFetcher.return_value
-  mock_fetcher.get_epitopes.return_value = dummy_epitopes
-  mock_fetcher.get_sources.return_value = dummy_sources
-
-  epitopes = mock_fetcher.get_epitopes('319224')
-  assert 'THEILWPSF' in epitopes['Sequence'].values
-
-  sources = mock_fetcher.get_sources('319224')
-  assert 'ABP77492.1' in sources['Accession'].values
-
-@patch('protein_tree.get_data.DataFetcher')
-def test_select_proteome(MockDataFetcher):
-  mock_fetcher = MockDataFetcher.return_value
-  mock_fetcher.get_epitopes.return_value = dummy_epitopes
-  epitopes = mock_fetcher.get_epitopes('319224')
-
+def test_select_proteome(epitopes):
+  epitopes_df = pd.read_csv(epitopes)
   selector = ProteomeSelector('24', 'Shewanella putrefaciens')
-  proteome_data = selector.select_best_proteome(epitopes)
+  proteome_data = selector.select_best_proteome(epitopes_df)
   assert proteome_data[0] == 'UP000008209'
-
