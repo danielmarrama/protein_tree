52fd1e85d13e4572e21d026c338447cd9ca10eba
Daniel Marrama <danielmarrama@gmail.com>
2023-07-17 19:40:17 -0700

    limit epitopes only to sources needed

--- protein_tree/get_data.py
+++ protein_tree/get_data.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import pandas as pd
 from pathlib import Path
 from sqlalchemy import text
@@ -8,16 +9,26 @@ from sql_engine import create_sql_engine
 
 
 class DataFetcher:
-  def __init__(self) -> None:
+  def __init__(self, all_taxa: str) -> None:
     self.sql_engine = create_sql_engine()
+    self.all_taxa = all_taxa.replace(';', ',')
 
-  def get_epitopes(self, all_taxa: str) -> pd.DataFrame:
-    """Get all epitopes for a species including all children taxa.
 
-    Args:
-      all_taxa: All children taxa for a species from the IEDB separated by ';'.
-    """
-    all_taxa = all_taxa.replace(';', ',')
+  def get_data(self) -> tuple:
+    """Get all epitopes and source antigens for a species together."""
+    epitopes_df = self._get_epitopes()
+    sources_df = self._get_sources()
+
+    # keep only epitopes whose source antigen is in sources_df
+    epitopes_df = epitopes_df[
+      epitopes_df['Source Accession'].isin(sources_df['Accession'])
+    ]
+
+    return epitopes_df, sources_df
+
+
+  def _get_epitopes(self) -> pd.DataFrame:
+    """Get all epitopes for a species."""
     sql_query1 = f"""
                   SELECT object.mol1_seq, object.region, object.mol2_name, 
                          object.mol2_accession
@@ -25,7 +36,7 @@ class DataFetcher:
                   WHERE epitope.e_object_id = object.object_id
                   AND object.object_sub_type IN 
                       ("Peptide from protein", "Discontinuous protein residues")
-                  AND object.organism2_id IN ({all_taxa});
+                  AND object.organism2_id IN ({self.all_taxa});
                   """
     sql_query2 = f"""
                   SELECT object.mol1_seq, object.region, object.mol2_name, 
@@ -34,7 +45,7 @@ class DataFetcher:
                   WHERE epitope.related_object_id = object.object_id
                   AND object.object_sub_type IN 
                       ("Peptide from protein", "Discontinuous protein residues")
-                  AND object.organism2_id IN ({all_taxa});
+                  AND object.organism2_id IN ({self.all_taxa});
                   """
     
     with self.sql_engine.connect() as connection:
@@ -63,15 +74,10 @@ class DataFetcher:
     
     return epitopes_df[['Sequence', 'Source Name', 'Source Accession']]
 
-  def get_sources(self, all_taxa: list) -> pd.DataFrame:
-    """Get all source antigens for a species including all children taxa.
-
-    Args:
-      all_taxa: List of all children taxa for a species from the IEDB.
-    """
-    all_taxa = all_taxa.replace(';', ',')
+  def _get_sources(self) -> pd.DataFrame:
+    """Get all source antigens for a species."""
     sql_query = f'SELECT accession, name, sequence '\
-                f'FROM source WHERE organism_id IN ({all_taxa});'
+                f'FROM source WHERE organism_id IN ({self.all_taxa});'
 
     with self.sql_engine.connect() as connection:
       result = connection.execute(text(sql_query))
@@ -83,13 +89,31 @@ class DataFetcher:
     sources_df['Length'] = sources_df['Sequence'].str.len()
     
     return sources_df
+    
+
+def run(taxon_id: int, all_taxa: str, species_name: str) -> None:
+  """Run the data fetching pipeline for a species.
+
+  Args:
+    taxon_id: Taxon ID for species to pull data for.
+    all_taxa: All children taxa for that species.
+    species_name: Name of the species.
+  """
+  epitopes_df, sources_df = DataFetcher(all_taxa).get_data()
+
+  # create directory for species and taxon ID
+  species_path = Path(__file__).parent.parent / 'species' / f'{taxon_id}-{species_name.replace(" ", "_")}'
+  os.makedirs(species_path, exist_ok=True)
+
+  # write epitopes and source antigens to files
+  epitopes_df.to_csv(species_path / 'epitopes.csv', index=False)
+  sources_df.to_csv(species_path / 'sources.csv', index=False)
+
 
 def main():
   import argparse
-  import os
-
-  parser = argparse.ArgumentParser()
   
+  parser = argparse.ArgumentParser()
   parser.add_argument(
     '-a', '--all_species',
     action='store_true',
@@ -100,45 +124,35 @@ def main():
     help='Taxon ID for species to pull data for.')
   
   args = parser.parse_args()
+
+  species_df = pd.read_csv(Path(__file__).parent.parent / 'species.csv')
+  valid_taxon_ids = species_df['Species Taxon ID'].tolist()
   
+  # create maps for taxon ID to species name and all taxa
+  species_name_map = dict(
+    zip(
+      species_df['Species Taxon ID'], 
+      species_df['Species Name']
+    )
+  )
+  all_taxa_map = dict(
+    zip(
+      species_df['Species Taxon ID'],
+      species_df['All Taxa']
+    )
+  )  
+
   if not args.all_species and not args.taxon_id:
-    print("Error: Please provide either --all_species or --taxon_id.")
+    print("Error: Please provide either --all_species (-a) or --taxon_id (-t).")
     return
 
-  Fetcher = DataFetcher()
   if args.all_species:
-    Fetcher.get_species_data()
+    for taxon_id in valid_taxon_ids:
+      run(taxon_id, all_taxa_map[taxon_id], species_name_map[taxon_id])
+
   else:
     taxon_id = args.taxon_id
-
-    parent_dir = Path(__file__).parent
-    species_df = pd.read_csv(parent_dir / '..' / 'species.csv')
-    
-    # create maps for taxon ID to species name and all taxa
-    species_name_map = dict(
-      zip(
-        species_df['Species Taxon ID'], 
-        species_df['Species Name']
-      )
-    )
-    all_taxa_map = dict(
-      zip(
-        species_df['Species Taxon ID'],
-        species_df['All Taxa']
-      )
-    )
-
-    # get epitopes and source antigens
-    epitopes_df = Fetcher.get_epitopes(all_taxa_map[taxon_id])
-    sources_df = Fetcher.get_sources(all_taxa_map[taxon_id])
-
-    # create directory for species and taxon ID
-    species_path = parent_dir / '..' / 'species' / f'{taxon_id}-{species_name_map[taxon_id].replace(" ", "_")}'
-    os.makedirs(species_path, exist_ok=True)
-
-    # write epitopes and source antigens to files
-    epitopes_df.to_csv(species_path / 'epitopes.csv', index=False)
-    sources_df.to_csv(species_path / 'sources.csv', index=False)
+    run(taxon_id, all_taxa_map[taxon_id], species_name_map[taxon_id])
 
 if __name__ == '__main__':
   main()
