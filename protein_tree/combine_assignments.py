8de180b4092a2b50302f6917ea7aff124e702999
Daniel Marrama <danielmarrama@gmail.com>
2024-01-22 16:12:11 -0800

    fix combine_assignments.py for new rearrangement

--- protein_tree/combine_assignments.py
+++ protein_tree/combine_assignments.py
@@ -1,63 +1,74 @@
 #!/usr/bin/env python3
 
-import os
+import argparse
 import pandas as pd
 from pathlib import Path
 
 
 def main():
-  """Combine all source antigen and epitope assignments into two TSV files."""
+  """Combine all peptide and peptide source assignments into two TSV files."""
+  
+  parser = argparse.ArgumentParser()
 
-  print('Combining all epitope and source assignments...\n')
+  parser.add_argument(
+    'build_path',
+    type=str,
+    default='build/',
+    help='Path to species directory.'
+  )
 
-  all_epitopes_df, all_sources_df = pd.DataFrame(), pd.DataFrame()
+  args = parser.parse_args()
+  build_path = Path(args.build_path)
 
-  data_path = Path(__file__).parent.parent / 'data'
-  for species in (data_path / 'species').iterdir():
+  print('Combining all peptide and source assignments...\n')
+
+  all_peptides_df, all_sources_df = pd.DataFrame(), pd.DataFrame()
+
+  for species in (build_path / 'species').iterdir():
     try:
-      epitopes_df = pd.read_csv(species / 'epitope_assignments.tsv', sep='\t')
-      sources_df = pd.read_csv(species / 'source_assignments.tsv', sep='\t')
+      peptides_df = pd.read_csv(species / 'peptide-assignments.tsv', sep='\t')
+      sources_df = pd.read_csv(species / 'source-assignments.tsv', sep='\t')
     except FileNotFoundError:
       continue
-    
-    # add taxon ID and species name columns
-    taxon_id = species.name.split('-')[0]
-    species_name = species.name.split('-')[1]
 
-    epitopes_df['Taxon ID'] = taxon_id
-    epitopes_df['Species Name'] = species_name
+    species_data = pd.read_csv(species / 'species-data.tsv', sep='\t')
+    taxon_id = species_data['Species Taxon ID'].iloc[0]
+    species_name = species_data['Species Name'].iloc[0]
 
-    sources_df['Taxon ID'] = taxon_id
+    peptides_df['Species Taxon ID'] = taxon_id
+    peptides_df['Species Name'] = species_name
+
+    sources_df['Species Taxon ID'] = taxon_id
     sources_df['Species Name'] = species_name
 
     # rearrange columns to put taxon ID and species name first
-    epitopes_df = epitopes_df[['Taxon ID', 'Species Name'] + epitopes_df.columns[:-2].tolist()]
-    sources_df = sources_df[['Taxon ID', 'Species Name'] + sources_df.columns[:-2].tolist()]
+    peptides_df = peptides_df[['Species Taxon ID', 'Species Name'] + peptides_df.columns[:-2].tolist()]
+    sources_df = sources_df[['Species Taxon ID', 'Species Name'] + sources_df.columns[:-2].tolist()]
 
-    all_epitopes_df = pd.concat([all_epitopes_df, epitopes_df])
+    all_peptides_df = pd.concat([all_peptides_df, peptides_df])
     all_sources_df = pd.concat([all_sources_df, sources_df])
 
   # write to files
-  all_epitopes_df.to_csv(data_path / 'all_epitope_assignments.tsv', sep='\t', index=False)
-  all_sources_df.to_csv(data_path / 'all_source_assignments.tsv', sep='\t', index=False)
+  all_peptides_df.to_csv(build_path / 'arborist' / 'all-peptide-assignments.tsv', sep='\t', index=False)
+  all_sources_df.to_csv(build_path / 'arborist' / 'all-source-assignments.tsv', sep='\t', index=False)
 
   # report total successful assignments
-  all_epitopes_df.drop_duplicates(subset=['Sequence', 'Source Accession'], inplace=True)
+  all_peptides_df.drop_duplicates(subset=['Sequence', 'Accession'], inplace=True)
   all_sources_df.drop_duplicates(subset=['Accession'], inplace=True)
 
   source_count = len(all_sources_df)
-  epitope_count = len(all_epitopes_df)
+  peptide_count = len(all_peptides_df)
 
   source_success_count = len(all_sources_df[all_sources_df['Assigned Protein ID'].notna()])
-  epitope_success_count = len(all_epitopes_df[all_epitopes_df['Assigned Protein ID'].notna()])
+  peptide_success_count = len(all_peptides_df[all_peptides_df['Assigned Protein ID'].notna()])
 
-  print(f'Total epitopes: {epitope_count}')
+  print(f'Total peptides: {peptide_count}')
   print(f'Total sources: {source_count}\n')
 
-  print(f'Total successful epitope assignments: {epitope_success_count}')
+  print(f'Total successful peptide assignments: {peptide_success_count}')
   print(f'Total successful source assignments: {source_success_count}\n')
 
-  print(f'Epitope success rate: {epitope_success_count / epitope_count * 100:.2f}%')
+  print(f'Peptide success rate: {peptide_success_count / peptide_count * 100:.2f}%')
   print(f'Source success rate: {source_success_count / source_count * 100:.2f}%')
 
 
