aff521a0665ed4f6e49e70d1f417b7880aab4449
Daniel Marrama <danielmarrama@gmail.com>
2024-01-23 20:08:38 -0800

    concat trees + outfile option

--- protein_tree/make_tree.py
+++ protein_tree/make_tree.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
-import math
+import os
 import pandas as pd
 from anytree import Node, RenderTree
 from pathlib import Path
 
 
-def build_tree_for_species(peptide_assignments_df):
-  gene_nodes = {}
+def build_tree_for_species(taxon_id: int) -> dict:
+  """Create a tree of assigned genes and proteins for a given species.
+
+  Args:
+    taxon_id: The taxon ID of the species to build the tree for."""
+
+  root = Node(f'Taxon ID: {taxon_id}')
+  tree = {root.name: root}
   protein_tracker = {}
+
+  peptide_assignments_df = pd.read_csv(build_path / 'species' / str(taxon_id) / 'peptide-assignments.tsv', sep='\t')
+  peptide_assignments_df['Assigned Gene'].fillna(peptide_assignments_df['ARC Assignment'], inplace=True)
 
   for _, row in peptide_assignments_df.iterrows():
     gene = row['Assigned Gene'] if not pd.isna(row['Assigned Gene']) else 'Unknown Gene'
@@ -20,16 +29,28 @@ def build_tree_for_species(peptide_assignments_df):
 
     protein_info = f"{protein_name} (UniProt: {protein_id})" if not pd.isna(protein_name) else f"UniProt: {protein_id}"
 
-    if gene not in gene_nodes:
-      gene_nodes[gene] = Node(gene)
+    if gene not in tree:
+      tree[gene] = Node(gene, parent=root)
       protein_tracker[gene] = set()
 
     if protein_info not in protein_tracker[gene]:
-      Node(protein_info, parent=gene_nodes[gene])
+      Node(protein_info, parent=tree[gene])
       protein_tracker[gene].add(protein_info)
 
-  return gene_nodes
+  return tree
 
+def write_tree_to_file(tree:dict, outfile: str) -> None:
+  """Write the tree to a file.
+
+  Args:
+    tree: The tree to write to file.
+    outfile: The file to write the tree to."""
+
+  with open(outfile, 'w') as f:
+    for taxon_id, tree in tree.items():
+      root_node = tree[f'Taxon ID: {taxon_id}']
+      for pre, _, node in RenderTree(root_node):
+        f.write("%s%s\n" % (pre, node.name))
 
 if __name__ == '__main__':
 
@@ -53,21 +74,32 @@ if __name__ == '__main__':
     type=int,
     help='Taxon ID for the species to pull data for.'
   )
+  parser.add_argument(
+    '-o', '--outfile', 
+    type=str,
+    help='Output file for tree.'
+  )
 
   args = parser.parse_args()
 
   build_path = Path(args.build_path)
   all_species = args.all_species
   taxon_id = args.taxon_id
+  outfile = args.outfile
 
+  tree = {}
   if all_species:
-    pass
+    for taxon_id in [139, 9593]:
+      tree[taxon_id] = build_tree_for_species(taxon_id)
 
   else:
-    peptide_assignments_df = pd.read_csv(build_path / 'species' / str(taxon_id) / 'peptide-assignments.tsv', sep='\t')
-    peptide_assignments_df['Assigned Gene'].fillna(peptide_assignments_df['ARC Assignment'], inplace=True)
-    tree = build_tree_for_species(peptide_assignments_df)
+    assert taxon_id is not None, 'Must provide a taxon ID if not building all species.'
+    tree[taxon_id] = build_tree_for_species(taxon_id)
 
-    for nodes in tree:
-      for pre, _, node in RenderTree(tree[nodes]):
+  if outfile:
+    write_tree_to_file(tree, outfile)
+  else:
+    for taxon_id, tree in tree.items():
+      root_node = tree[f'Taxon ID: {taxon_id}']
+      for pre, _, node in RenderTree(root_node):
         print("%s%s" % (pre, node.name))
